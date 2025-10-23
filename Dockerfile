59b8c6069e38845c6bf10f2c2c65c92b08af1ff6
Cesar Munoz <cesarmu@hotmail.com>
2025-10-23 00:12:21 -0500

    Se agrega application-dev para dejar el ambiente de desarrollo,se modifica application.yml para dejar ahi las variables comunes, se agrega build-docker.sh para cambiar el modo de despliegue por medio de docker-compose y archivo .env para los secretos, se modifica el dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,26 +1,17 @@
-# Etapa 1: Compilación del proyecto
+# Etapa 1: Compilación
 FROM maven:3.8.6-eclipse-temurin-17 AS build
 WORKDIR /app
-
-# Copiamos los archivos necesarios para construir el proyecto
+# Copiar archivos necesarios para compilar
 COPY pom.xml .
 COPY src ./src
-
-# Construimos el proyecto y empaquetamos el JAR
+# Compilar y crear el JAR
 RUN mvn clean package -DskipTests
-
-# Etapa 2: Construcción de la imagen para producción
+# Etapa 2: Construcción para stage
 FROM eclipse-temurin:17-jre
 WORKDIR /app
-
-# Copiar el JAR desde la etapa de compilación
+# Copiar el JAR desde la etapa anterior
 COPY --from=build /app/target/emqx-to-rabbit-0.0.1-SNAPSHOT.jar app.jar
-
-# Exponemos el puerto que usa tu aplicación
+# Exponer el puerto
 EXPOSE 8080
-
-# Variables de entorno (estas pueden ser sobreescritas con --env-file)
-ENV SPRING_PROFILES_ACTIVE=stage
-
-# Comando para ejecutar la aplicación
+# Ejecutar la aplicación
 ENTRYPOINT ["java", "-jar", "app.jar"]
---
+++ build-docker.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#Variables
+APP_NAME="emqx-to-rabbit-stage"
+DOCKER_TAG="bitiot21/$APP_NAME"
+IMAGE_VERSION="1.0.0" # En vez de versiones, se puede usar latest, pero esta será la versión 1 del proyecto, entonces así
+
+echo "Iniciando build y push para $APP_NAME..."
+
+# 1. Construyendo el JAR universal con Maven
+echo "Paso 1. Compilando el proyecto con Maven..."
+mvn clean package -DskipTests || { echo "Error en la compilación con Maven"; exit 1; }
+
+# 2. Construir la imagen Docker
+echo "Paso 2. Construyendo la imagen Docker: $DOCKER_TAG:$IMAGE_VERSION"
+docker build -t $DOCKER_TAG:$IMAGE_VERSION . || { echo "Error al construir la imagen Docker"; exit 1; }
+
+# 3. Subir la imagen a Docker Hub
+echo " Paso 3. Haciendo PUSH de la imagen Docker a Docker Hub..."
+docker push $DOCKER_TAG:$IMAGE_VERSION || { echo "Error al subir la imagen a Docker Hub"; exit 1; }
+
+echo "¡Proceso finalizado! Imagen $DOCKER_TAG:$IMAGE_VERSION subida."
+
+# Opcional: preguntar antes de cerrar
+read -p "Presiona enter para continuar..."
